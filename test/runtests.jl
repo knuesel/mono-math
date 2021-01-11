0d7fdb2d0632014519dee848bb4c746a98b8dba4
Jeremie Knuesel <knuesel@gmail.com>
2021-01-11 19:10:07 +0100

    Add test for invalid Unicode codepoints

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using MonoMath
 using Test
+using Unicode
 
-@testset "MonoMath.jl" begin
-    # Write your tests here.
+@testset "MonoMath" begin
+  # Check that all codepoints used in the samples are valid Unicode
+  for (i, s) in enumerate(MonoMath.text_samples)
+    @testset "Sample $i" begin
+      for (j, c) in enumerate(s)
+        @testset "Codepoint $j ($c = $(repr(codepoint(c))))" begin
+          @test Unicode.isassigned(c)
+        end
+      end
+    end
+  end
 end
